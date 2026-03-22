d166783378e7d387ef3953a348bab836bec02da2
Grzegorz Brzezina <gbrzezina24@gmail.com>
2026-03-22 19:59:18 +0100

    Initial commit

---
+++ Pages/Student/ChoosePromoter.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PromotorSelection.Pages.Student
+{
+    public class ChoosePromoterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
